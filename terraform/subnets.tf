d5f7cb669cec0d86c97a65b2225b580fc6cbe60c
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2023-08-17 14:23:40 -0700

    added subnets

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "public_1"{
 resource "aws_subnet" "public_2"{
     vpc_id = aws_vpc.vpc.id
     cidr_block = var.public_subnets[1]
-    availability_zone = var.availability_zone[1]
+    availability_zone = var.availability_zones[1]
     map_public_ip_on_launch = true
 
     tags={
