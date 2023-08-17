fef0e3f58790ee5b39a1d7cb39660ad26b238400
陈梦恬 <mengtianchen@s-MacBook-Air.lan>
2023-08-17 13:59:01 -0700

    vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,15 @@
+/*the entry point,describe what provider we are using 
+and where we are getting our terraform state from
+ and give terraform everything it needs to know to excute its code*/
+/*WE USE TERRAFORM VODE TO CREATE AWS INFRASTRUCTURE*/
 terraform {
     required_providers {
         aws = {
             source ="hashicorp/aws"
-            version = "-> 4.0"
+            version = "~> 4.0"
         }
     }
-    backend "s3" {}
+    backend "s3" {} //where our Terraform state is
 }
 
 provider "aws"{
